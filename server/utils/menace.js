ccb4455a9a80eb7816cca6dfa11374776255c20d
Isaac Pillar <isaac.pillar@icloud.com>
2022-11-09 17:30:04 +0000

    feat: implement function to add beads to state and another to let MENACE play given a board

--- server/utils/menace.js
+++ server/utils/menace.js
@@ -1,7 +1,34 @@
 
 
-function createMENACE () {
+function createMENACE (name, win = 3, lose = -1, draw = 1) {
+  const states = generateStates();
+  return {name, states, incentives: {
+    win,
+    lose,
+    draw
+  }};
+}
 
+function menacePlay (board, menace) {
+  const transformed = transformBoard(board, true);
+  const transformedBeads = menace.states[transformed[0]];
+  const beads = inverseTransform(transformedBeads, transformed[1], transformed[2]);
+  const totals = beads.split('').map(str => Number(str));
+  let total = 0;
+  for (let i = 0; i < 9; i++) {
+    total += Number(beads[i]);
+  }
+  let randomChoice = Math.floor(Math.random()*total);
+  console.log({randomChoice, totals, total});
+  let chosenIndex = 0;
+  while (randomChoice > 0) {
+    if (totals[chosenIndex] === 0) chosenIndex++;
+    else {
+      totals[chosenIndex]--;
+      randomChoice--;
+    }
+  }
+  return chosenIndex;
 }
 
 function checkWin (board) {
@@ -24,7 +51,7 @@ function checkWin (board) {
 
 function generateStates () {
   const temp = ['000000000'];
-  const out = ['000000000'];
+  const out = {'000000000': addBeads('000000000')};
   while (temp.length) {
     let current = temp.shift();
     let numbers = {0:[], 1:0, 2:0};
@@ -41,17 +68,62 @@ function generateStates () {
       if (checkWin(newState)) continue;
       newState = transformBoard(newState);
       if (temp.includes(newState)) continue;
-      if (numbers[0].length > 1) {
-        temp.push(newState);
-        out.push(newState);
-      }
+      if (numbers[0].length === 0) continue;
+      temp.push(newState);
+      out[newState] = addBeads(newState);
     }
   }
   return out;
 }
 
-function transformBoard (board) { // Each board state is given as a string '012120200' where 0 is empty, 1 is X and 2 is O
-  let largest = board;         // To store the state we rotate the board until we get the largest base 3 number
+function addBeads(state) { // TODO: Deal with beade symmetries
+  const zeros = [];
+  for (let i = 0; i < 9; i++) {
+    if (state[i] === '0') zeros.push(i);
+  }
+  const out = Array(9).fill(0);
+  for (let index of zeros) {
+    out[index] = zeros.length;
+  }
+  return out;
+}
+function transform (board, rotation, flip) {
+  const temp = [];
+  for (let position of rotation) {
+    temp.push(board[position]);
+  }
+  const out = [];
+  for (let position of flip) {
+    out.push(temp[position]);
+  }
+  return out.join('');
+}
+
+function inverseTransform (board, oldRotation, flip) {
+  const temp = [];
+  for (let position of flip) {
+    temp.push(board[position]);
+  }
+  const out = [];
+  const rotation = inverseRotation(oldRotation);
+  for (let position of rotation) {
+    out.push(temp[position]);
+  }
+  return out.join('');
+}
+
+function inverseRotation (rotation) {
+  const inverses = {
+    '012345678': [0,1,2,3,4,5,6,7,8],
+    '630741852': [2,5,8,1,4,7,0,3,6],
+    '876543210': [8,7,6,5,4,3,2,1,0],
+    '258147036': [6,3,0,7,4,1,8,5,2]
+  }
+  return inverses[rotation.join('')];
+}
+
+function transformBoard (board, show = false) { // Each board state is given as a string '012120200' where 0 is empty, 1 is X and 2 is O
+  let largest = [board, [0,1,2,3,4,5,6,7,8], [0,1,2,3,4,5,6,7,8]];         // To store the state we rotate the board until we get the largest base 3 number
   const rotations = [
     [0,1,2,3,4,5,6,7,8],
     [6,3,0,7,4,1,8,5,2],
@@ -63,24 +135,16 @@ function transformBoard (board) { // Each board state is given as a string '0121
     [2,1,0,5,4,3,8,7,6]
   ]
 
-  function transform (board, rotation, flip) {
-    const temp = [];
-    for (let position of rotation) {
-      temp.push(board[position]);
-    }
-    const out = [];
-    for (let position of flip) {
-      out.push(temp[position]);
-    }
-    return out.join('');
-  }
-
   for (let flip of flips) {
     for (let rotation of rotations) {
       const boardTra = transform(board, rotation, flip)
       const boardTernary = Number(Number(boardTra).toString(3));
-      if (boardTernary > Number(Number(largest).toString(3))) largest = boardTra;
+      if (boardTernary > Number(Number(largest[0]).toString(3))) largest = [boardTra, rotation, flip];
     }
   }
-  return largest;
-}
+  if (show) return largest;
+  return largest[0];
+};
+
+const nigel = createMENACE('Nigel');
+console.log(menacePlay('002021001', nigel));
