1f295f8e9e466155de2d7f972ada26614dd68622
Josep Abellana <josep.abe.pu@gmail.com>
2022-11-21 17:20:12 +0000

    feat(prevent deleting ais within training and battleting): prevent deleting ais in circumstances

--- client/tactic-toe/src/app/ai-info/ai-info.component.ts
+++ client/tactic-toe/src/app/ai-info/ai-info.component.ts
@@ -1,18 +1,20 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { APIClientService } from '../apiclient.service';
- 
+import { Location } from '@angular/common';
+
 @Component({
   selector: 'app-ai-info',
   templateUrl: './ai-info.component.html',
   styleUrls: ['./ai-info.component.css']
 })
 export class AiInfoComponent implements OnInit {
-
+  flag:boolean = false;
   @Input() ai = {id: 1, name: 'Bruce', results: {win:1, draw: 2, lose: 700}, color:'black'};
 
-  constructor(private api: APIClientService) { }
+  constructor(private api: APIClientService, private location: Location) { }
 
   ngOnInit(): void {
+    this.flag = this.location.path() === '/yourai';
   }
 
   deleteAi(){
